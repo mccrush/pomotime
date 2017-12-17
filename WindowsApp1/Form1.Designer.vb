11527cb3a2c5ca824c6acf7fbf45fbb87231cd33
mccrush <mccrush2027@gmail.com>
2017-12-17 10:24:16 +0700

    Обновляет значения текста на елементах формы. Переименовывает переменные

--- WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/Form1.Designer.vb
@@ -40,7 +40,6 @@ Partial Class Form1
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
         Me.Label7 = New System.Windows.Forms.Label()
         Me.ContextMenuStrip1.SuspendLayout()
         Me.SuspendLayout()
@@ -52,7 +51,7 @@ Partial Class Form1
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(150, 50)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Start Pomodoro"
+        Me.Button1.Text = "Start pomo time"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Label1
@@ -60,11 +59,11 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label1.Location = New System.Drawing.Point(57, 29)
+        Me.Label1.Location = New System.Drawing.Point(7, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(59, 25)
+        Me.Label1.Size = New System.Drawing.Size(162, 25)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "24 m"
+        Me.Label1.Text = "Pomodoro time!"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         Me.Label1.Visible = False
         '
@@ -74,7 +73,7 @@ Partial Class Form1
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(150, 50)
         Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Pause"
+        Me.Button2.Text = "Pause pomo time"
         Me.Button2.UseVisualStyleBackColor = True
         Me.Button2.Visible = False
         '
@@ -120,7 +119,7 @@ Partial Class Form1
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(150, 25)
         Me.Button3.TabIndex = 3
-        Me.Button3.Text = "Short break"
+        Me.Button3.Text = "Start short break"
         Me.Button3.UseVisualStyleBackColor = True
         Me.Button3.Visible = False
         '
@@ -130,7 +129,7 @@ Partial Class Form1
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(150, 25)
         Me.Button4.TabIndex = 4
-        Me.Button4.Text = "Long break"
+        Me.Button4.Text = "Start long break"
         Me.Button4.UseVisualStyleBackColor = True
         Me.Button4.Visible = False
         '
@@ -138,7 +137,7 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(26, 9)
+        Me.Label2.Location = New System.Drawing.Point(31, 34)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(120, 20)
         Me.Label2.TabIndex = 5
@@ -149,54 +148,50 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(182, 9)
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(50, 138)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(109, 20)
+        Me.Label3.Size = New System.Drawing.Size(59, 25)
         Me.Label3.TabIndex = 6
-        Me.Label3.Text = "Time to break:"
+        Me.Label3.Text = "24 m"
         Me.Label3.Visible = False
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(213, 29)
+        Me.Label4.Location = New System.Drawing.Point(169, 9)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(47, 25)
+        Me.Label4.Size = New System.Drawing.Size(149, 25)
         Me.Label4.TabIndex = 7
-        Me.Label4.Text = "5 m"
+        Me.Label4.Text = "Time to break!"
         Me.Label4.Visible = False
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(168, 110)
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(175, 33)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(149, 25)
+        Me.Label5.Size = New System.Drawing.Size(109, 20)
         Me.Label5.TabIndex = 8
-        Me.Label5.Text = "Time to break!"
+        Me.Label5.Text = "Time to break:"
         Me.Label5.Visible = False
         '
         'Label6
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(7, 110)
+        Me.Label6.Location = New System.Drawing.Point(223, 130)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(162, 25)
+        Me.Label6.Size = New System.Drawing.Size(47, 25)
         Me.Label6.TabIndex = 9
-        Me.Label6.Text = "Pomodoro time!"
-        '
-        'Timer2
-        '
-        Me.Timer2.Interval = 1000
+        Me.Label6.Text = "5 m"
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(62, 143)
+        Me.Label7.Location = New System.Drawing.Point(69, 184)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(39, 13)
         Me.Label7.TabIndex = 10
@@ -207,7 +202,7 @@ Partial Class Form1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Control
-        Me.ClientSize = New System.Drawing.Size(321, 168)
+        Me.ClientSize = New System.Drawing.Size(313, 209)
         Me.ControlBox = False
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
@@ -254,6 +249,5 @@ Partial Class Form1
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
-    Friend WithEvents Timer2 As Timer
     Friend WithEvents Label7 As Label
 End Class
--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,39 +1,18 @@
 ﻿Public Class Form1
 
-    Public pomoTime As Single
-    Public breakTime As Single
-    Public longPause As Single
-    Public statusShow As Boolean = True
-    Public tipeTime As Int32
+    Public pomoTime As Integer = My.Settings.pt * 60
+    Public shortBreakTime As Integer = My.Settings.st * 60
+    Public longBreakTime As Integer = My.Settings.lt * 60
+    Public statusFormView As Boolean = True
+    Public typeTime As Int32 = 1
 
     'Загрузка формы
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        pomoTime = My.Settings.pt * 60
-    End Sub
-
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Timer1.Start()
-        tipeTime = 1
-
+        Button1.Show()
         Label1.Show()
-        Label2.Show()
-        Button2.Show()
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        If (tipeTime = 1) Then
-            Timer1.Stop()
-
-            Button1.Show()
-        Else
-            Timer2.Stop()
-
-            Button3.Show()
-            Button4.Show()
-        End If
-    End Sub
-
+    'Таймер в ктором происходит самое главное
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Dim minuts, seconds As Integer
         Dim nullSec As String
@@ -51,67 +30,62 @@
         minuts = Int(pomoTime / 60)
         seconds = pomoTime Mod 60
         nullSec = If(pomoTime Mod 60 >= 10, "", "0")
-
         Label1.Text = minuts & ":" & nullSec & seconds & " m"
     End Sub
 
-    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        Dim minuts, seconds As Integer
-        Dim nullSec As String
-        breakTime = breakTime - 1
-
-        If (breakTime < 1) Then
-            Timer2.Stop()
-
-            Label6.Show()
-            Button1.Show()
-        End If
-
-        minuts = Int(breakTime / 60)
-        seconds = breakTime Mod 60
-        nullSec = If(breakTime Mod 60 >= 10, "", "0")
-
-        Label4.Text = minuts & ":" & nullSec & seconds & " m"
+    'События по нажатию на каждую из кнопок
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+        typeTime = 1
+        Label2.Show()
+        Label3.Show()
+        Button2.Show()
     End Sub
 
-    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
-        Me.Close()
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (typeTime = 1) Then
+            Timer1.Stop()
+            Button1.Show()
+        Else
+            Button3.Show()
+            Button4.Show()
+        End If
+    End Sub
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    'Работа со второй формой и контекстным меню
+    Private Sub NotifyIcon1_MouseClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseClick
+        If (e.Button = MouseButtons.Left) Then
+            If (statusFormView) Then
+                Me.Hide()
+                statusFormView = False
+            Else
+                Me.Show()
+                statusFormView = True
+            End If
+        End If
     End Sub
 
     Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
         Form2.Show()
     End Sub
 
-    Private Sub NotifyIcon1_MouseClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseClick
-        If (e.Button = MouseButtons.Left) Then
-            If (statusShow) Then
-                Me.Hide()
-                statusShow = False
-            Else
-                Me.Show()
-                statusShow = True
-            End If
-        End If
-    End Sub
-
     Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
         AboutBox1.Show()
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        breakTime = My.Settings.st * 60
-        Timer2.Start()
-        tipeTime = 2
-
-        Button2.Show()
-    End Sub
-
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        breakTime = My.Settings.lt * 60
-        Timer2.Start()
-        tipeTime = 2
-
-        Button2.Show()
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
     End Sub
 
 End Class
